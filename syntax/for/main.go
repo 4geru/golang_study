bb3d42a614c208dce4bb791b314a0acccc8f1abd
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-04-04 23:30:58 +0900

    test syntax

--- syntax/for/main.go
+++ syntax/for/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 
 	numbers := make([]*int, 0, 3)
-	for i := 0; i < 3; i++ {
-		numbers = append(numbers, &i)
-	}
+  for i := 0; i < 3; i++ {
+    var v int = i
+		numbers = append(numbers, &v)
+  }
 
 	fmt.Println("Values:", *numbers[0], *numbers[1], *numbers[2])
 	fmt.Println("Addresses:", numbers[0], numbers[1], numbers[2])
--- syntax/pointer/main.go
+++ syntax/pointer/main.go
@@ -11,15 +11,18 @@ func main() {
 	returnValue := increment(n)
 	fmt.Printf("Value of n is %d\n", n)
 	fmt.Printf("Return Value of increment is %d\n", returnValue)
+	fmt.Printf("address check %b\n", &n == &returnValue)
 	// 参照渡し
 	// 戻り値を受け取らなくても渡した変数が書き換わる
-	incrementWithPointer(&n)
+	pointerReturnValue := incrementWithPointer(&n)
 	fmt.Printf("Value of n is %d\n", n)
+	fmt.Printf("address check %b\n", &n == pointerReturnValue)
 }
 
 func increment(n int) int {
 	return n + 1
 }
-func incrementWithPointer(n *int) {
-	*n++
+func incrementWithPointer(n *int) *int {
+  *n++
+  return n
 }
